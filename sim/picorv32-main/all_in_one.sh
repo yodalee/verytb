64d5cdb25bb8e6e00297b49a04c3f02f6af2477f
Yu-Sheng Lin <johnjohnlys@gmail.com>
2025-06-08 13:45:12 +0800

    Add very simple TB

--- sim/picorv32-main/all_in_one.sh
+++ sim/picorv32-main/all_in_one.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 function Verilate() {
-	verilator --cc picorv32.v --Mdir Vpicorv32_axi --trace-fst --top-module picorv32_axi
-	pushd picorv32_axi
+	verilator --cc picorv32.v --Mdir Vpicorv32_axi --trace-fst --top-module picorv32_axi -CFLAGS "-mbmi2"
+	pushd Vpicorv32_axi
 	make -j -f Vpicorv32_axi.mk
 	popd
 }
 
 function CompileMain() {
+	g++ -L Vpicorv32_axi -I /usr/share/verilator/include -mbmi2 main.cpp -lz -lVpicorv32_axi -lverilated
 }
 
 Verilate
--- sim/picorv32-main/main.cpp
+++ sim/picorv32-main/main.cpp
@@ -1,58 +1,273 @@
-#pragma once
 // Direct include
 #include "Vpicorv32_axi/Vpicorv32_axi.h"
 // C system headers
 // C++ standard library headers
+#include <deque>
+#include <fstream>
+#include <iostream>
 #include <memory>
+#include <optional>
 // Other libraries' .h files.
+#include "immintrin.h"
+#include "verilated_fst_c.h"
 // Your project's .h files.
 
 using namespace std;
 
-struct Vpicorv32Wrap {
-	unique_ptr<Vpicorv32_axi> dut;
-	Vpicorv32Wrap():
-		dut(new Vpicorv32_axi)
-	{
-		dut->resetn = 1;
-		PosNeg();
-		PosNeg();
-		dut->resetn = 0;
-		PosNeg();
-		PosNeg();
-		dut->resetn = 1;
-		PosNeg();
-		PosNeg();
+struct ar_s {
+	uint8_t prot;
+	uint32_t addr;
+};
+using aw_s = ar_s;
 
+struct r_s {
+	uint32_t data;
+};
+
+struct w_s {
+	uint32_t data;
+	uint8_t strb;
+};
+
+struct b_s {
+};
+
+template <typename T>
+void OptionalToQueue(optional<T>& opt, deque<T>& dq, unsigned max_size) {
+	if (opt.has_value() and dq.size() < max_size) {
+		dq.push_back(move(*opt));
+		opt.reset();
+	}
+}
+
+template <typename T>
+void QueueToOptional(deque<T>& dq, optional<T>& opt) {
+	if (!opt.has_value() && !dq.empty()) {
+		opt = move(dq.front());
+		dq.pop_front();
+	}
+}
+
+void BitMaskToByteMask(uint32_t& byte_mask, uint8_t bit_mask) {
+	byte_mask = _pdep_u32(bit_mask, 0x1010'1010u);
+	byte_mask -= byte_mask >> 4;
+	byte_mask |= byte_mask << 4;
+}
+
+void BitMaskToByteMask(uint64_t& byte_mask, uint8_t bit_mask) {
+	byte_mask = _pdep_u64(bit_mask, 0x1010'1010'1010'1010llu);
+	byte_mask -= byte_mask >> 4;
+	byte_mask |= byte_mask << 4;
+}
+
+struct Vpicorv32Wrap {
+	optional<ar_s>* ar;
+	optional<aw_s>* aw;
+	optional<r_s>* r;
+	optional<w_s>* w;
+	optional<b_s>* b;
+	unsigned *irq;
+
+private:
+	VerilatedContext ctx;
+	VerilatedFstC tfp;
+	Vpicorv32_axi dut;
+	unsigned timestamp = 0;
+
+public:
+
+	Vpicorv32Wrap() {
+		ctx.traceEverOn(true);
+		dut.trace(&tfp, 99);
+		tfp.open("dump.fst");
+
+		dut.resetn = 1;
+		Eval();
+		Eval();
+		dut.resetn = 0;
+		Eval();
+		Eval();
+		dut.resetn = 1;
+		Eval();
+		Eval();
 	}
 
-	void PosNeg() {
-		dut->clk = 0;
-		dut->eval();
-		dut->clk = 1;
-		dut->eval();
+	~Vpicorv32Wrap() {
+		tfp.close();
+	}
+
+	void Eval() {
+		dut.clk = 0;
+		dut.eval();
+		tfp.dump(timestamp++);
+		dut.clk = 1;
+		dut.eval();
+		tfp.dump(timestamp++);
 	}
 
 	void Pre() {
+		// better to use RAII to manage
+
+		// Only Valid/Ready input and signal out are put here
+		dut.mem_axi_rvalid = r->has_value();
+		if (dut.mem_axi_rvalid and dut.mem_axi_rready) {
+			cout << "Get R" << endl;
+			dut.mem_axi_rdata = r->value().data;
+			r->reset();
+		}
+		dut.mem_axi_bvalid = b->has_value();
+		if (dut.mem_axi_bvalid and dut.mem_axi_bready) {
+			b->reset();
+		}
 	}
 
 	void Post() {
-		dut->clk = 0;
-		dut->eval();
-		dut->clk = 1;
-		dut->eval();
+		// better to use RAII to manage
+		bool arready = not ar->has_value();
+		bool awready = not aw->has_value();
+		bool wready = not w->has_value();
+		bool arvalid = dut.mem_axi_arvalid;
+		bool awvalid = dut.mem_axi_awvalid;
+		bool wvalid = dut.mem_axi_wvalid;
+		dut.mem_axi_arready = arready;
+		dut.mem_axi_awready = awready;
+		dut.mem_axi_wready = wready;
+		Eval();
+		if (arvalid and arready) {
+			cout << "Send AR" << endl;
+			*ar = {dut.mem_axi_arprot, dut.mem_axi_araddr};
+		}
+		if (awvalid and awready) {
+			*aw = {dut.mem_axi_awprot, dut.mem_axi_awaddr};
+		}
+		if (wvalid and wready) {
+			*w = {dut.mem_axi_wdata, dut.mem_axi_wstrb};
+		}
 	}
 };
 
 struct AxiMemory {
+	optional<ar_s>* ar;
+	optional<aw_s>* aw;
+	optional<r_s>* r;
+	optional<w_s>* w;
+	optional<b_s>* b;
+	vector<unsigned> memory_space;
+
+private:
+	deque<ar_s> ar_q;
+	deque<aw_s> aw_q;
+	deque<r_s> r_q;
+	deque<w_s> w_q;
+	deque<b_s> b_q;
+
+public:
+
 	AxiMemory() {
 	}
-	void Main() {
+
+	void HandleInput_() {
+		OptionalToQueue(*ar, ar_q, 128);
+		OptionalToQueue(*aw, aw_q, 128);
+		OptionalToQueue(*w, w_q, 128);
+	}
+
+	void Exec_() {
+		if (not ar_q.empty() and r_q.size() < 16) {
+			auto& ar_data = ar_q.front();
+
+			r_q.push_back({memory_space[ar_data.addr / 4]});
+
+			ar_q.pop_front();
+		}
+		if (not aw_q.empty() and not w_q.empty() and b_q.size() < 16) {
+			auto& aw_data = aw_q.front();
+			auto& w_data = w_q.front();
+
+			uint32_t& dst = memory_space[aw_data.addr / 4];
+			uint32_t src = w_data.data;
+			uint32_t mask;
+			BitMaskToByteMask(mask, w_data.strb);
+			dst = (src & mask) | (dst & ~mask);
+
+			b_q.emplace_back();
+			aw_q.pop_front();
+			w_q.pop_front();
+		}
+	}
+
+	void HandleInputAndExec() {
+		HandleInput_();
+		Exec_();
+	}
+
+	void HandleOutput() {
+		QueueToOptional(r_q, *r);
+		QueueToOptional(w_q, *w);
 	}
 };
 
+vector<unsigned> ReadBin(const string& filename) {
+	vector<unsigned> data;
+	ifstream file(filename, ios::binary | ios::ate);
+	if (not file) {
+		cerr << "Failed to open file: " << filename << endl;
+		abort();
+	}
+
+	streamsize size = file.tellg();
+	if (size % sizeof(unsigned) != 0) {
+		cerr << "File size is not aligned to 4 bytes." << endl;
+		abort();
+	}
+
+	file.seekg(0, ios::beg);
+	size_t count = size / sizeof(unsigned);
+	data.resize(count);
+
+	if (not file.read(reinterpret_cast<char *>(data.data()), size)) {
+		cerr << "Failed to read data from file." << endl;
+		abort();
+	}
+
+	return data;
+}
+
 int main() {
+	// Channe
+	optional<ar_s> ar;
+	optional<aw_s> aw;
+	optional<r_s> r;
+	optional<w_s> w;
+	optional<b_s> b;
+	unsigned irq;
+
+	// Module
 	Vpicorv32Wrap dut;
 	AxiMemory axi;
+	axi.memory_space = ReadBin("firmware.bin");
+
+	// Connect
+	dut.ar = &ar;
+	dut.aw = &aw;
+	dut.r = &r;
+	dut.w = &w;
+	dut.b = &b;
+	dut.irq = &irq;
+
+	axi.ar = &ar;
+	axi.aw = &aw;
+	axi.r = &r;
+	axi.w = &w;
+	axi.b = &b;
+
+	// main simulation kernel
+	for (unsigned i = 0; i < 600; ++i) {
+		axi.HandleInputAndExec();
+		dut.Pre();
+
+		axi.HandleOutput();
+		dut.Post();
+	}
 	return 0;
 }
